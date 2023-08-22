01788c0e6f90dcf9337503611acdd0d9b7bfda47
Jimmy Zelinskie <jimmy@zelinskie.com>
2023-08-21 20:12:30 -0400

    basic highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,23 @@
+; highlights.scm
+
+[
+  "permission"
+  "relation"
+  "definition"
+] @keyword
+
+[
+  "|"
+  "+"
+  "-"
+  "&"
+  "#"
+  "->"
+  "="
+] @operator
+
+[
+ "nil"
+] @constant.builtin
+
+(comment) @comment

624c48ca77d54f97d8e8b31a4cdfc239a6d5b955
Jimmy Zelinskie <jimmy@zelinskie.com>
2026-08-19 14:47:55 -0700

    include CEL syntax in injections

--- queries/injections.scm
+++ queries/injections.scm
@@ -2,4 +2,5 @@
  (#set! injection.language "comment"))
 
 ((caveat_expr) @injection.content
- (#set! injection.language "cel"))
+ (#set! injection.language "cel")
+ (#set! injection.include-children))
